285f774088618743bbc6852f318e095e54d4f3fd
korik1 <koriknarvaez@gmail.com>
2016-03-31 12:03:18 -0700

    Create GeneratorXML  v1

---
+++ api-server/modules/repository/lib/generatorXML.js
@@ -0,0 +1,9 @@
+http.createServer(function (req, resp) {
+  if (req.url === '/doodle.png') {
+    if (req.method === 'PUT') {
+      req.pipe(request.put('http://mysite.com/doodle.png'))
+    } else if (req.method === 'GET' || req.method === 'HEAD') {
+      request.get('http://mysite.com/doodle.png').pipe(resp)
+    }
+  }
+})
